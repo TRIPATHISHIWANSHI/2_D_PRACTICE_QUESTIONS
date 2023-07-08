8d3ddab4207df645e734e20c7560dcf21c2cc169
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-07-09 01:04:51 +0530

    Create setMatrixZero.java

---
+++ setMatrixZero.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+      
+        int m = matrix.length;
+        int n = matrix[0].length;
+      
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(row.contains(i) || col.contains(j)){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
